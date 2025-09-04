eefe36124133c5b7ace4e90a34bcfca44f334b73
marceloriv <ma.riveracontreras@duocuc.cl>
2025-09-04 18:07:38 -0400

    Refactor carga de componentes a main.js y eliminar scripts inline en index.html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,3 +5,27 @@
  * cargar el footer y header
  * ...cosas
  */
+
+/* Loader de componentes comunes */
+(function () {
+  function loadComponent(target, url, cb) {
+    const $el = $(target);
+    if (!$el.length) return;
+    $el.load(url, function (_, status) {
+      if (status !== 'success') {
+        console.warn('No se pudo cargar:', url);
+      }
+      cb && cb();
+    });
+  }
+
+  $(function () {
+    // ! recordar que hay que renombrar los archivos si es que cambian de lugar o nombre
+    loadComponent('#mainNav', 'components/mainNav.html', function () {
+      if (window.PowerFit && typeof PowerFit.fixNavLinks === 'function') {
+        PowerFit.fixNavLinks();
+      }
+    });
+    loadComponent('#header', 'components/header.html');
+  });
+})();
